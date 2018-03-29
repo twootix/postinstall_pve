67358825f452cf57e5a70b3c4ae1e97c3b471b94
twootix <37909033+twootix@users.noreply.github.com>
2018-03-29 18:35:44 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -142,7 +142,7 @@ echo "Nouveau serveur Proxmox {hostname -f} vient d'être installé.\n A mettre 
 sed -i.bak 's/NotFound/Active/g' /usr/share/perl5/PVE/API2/Subscription.pm
 
 ##modification du bashrc pour notification de connexion ssh avec l'history
-echo -e "echo 'Avertissement! Connexion au serveur :' \`hostname\` 'le:' \`date +'%Y/%m/%d'\` \`who | grep -v localhost\` | mail -s \"[ \`hostname\` ] Avertissement!!! connexion au serveur le: \`date +'%Y/%m/%d'\` \`who | grep -v localhost | awk {'print $5'}\`\"$adminmail\"" >> /etc/bash.bashrc
+echo -e "echo 'Avertissement! Connexion au serveur :' \`hostname\` 'le:' \`date +'%Y/%m/%d'\` \`who | grep -v localhost\` | mail -s \"[ \`hostname\` ] Avertissement!!! connexion au serveur le: \`date +'%Y/%m/%d'\` \`who\`\" \"$adminmail\"" >> /etc/bash.bashrc
 echo -e "PROMPT_COMMAND='history -a >(logger -t \"\$USER[\$PWD] \$SSH_CONNECTION\")'\"" >> /etc/bash.bashrc
 
 ##script Terminer
